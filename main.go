6b5b340cd7912096f78229bbc71b6882d4d2879d
İsmail ÇELEN <81293395+IsmailCLN@users.noreply.github.com>
2024-10-15 23:49:44 +0300

    test commit

--- main.go
+++ main.go
@@ -8,7 +8,7 @@ import (
 func home(w http.ResponseWriter, r *http.Request) { w.Write([]byte("Hello from Snippetbox")) }
 
 func snippetView(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Display a specific snippet..."))
+	w.Write([]byte("Display a specific snippet."))
 }
 
 func snippetCreate(w http.ResponseWriter, r *http.Request) {
